687a6ad3cbda03ac30bdbbf8e931ba3ed3f2300a
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-06-15 12:38:00 -0400

    added immutability

--- src/app/index.js
+++ src/app/index.js
@@ -30,26 +30,64 @@ import {render} from "react-dom";
 
 // render(<App />, window.document.getElementById('app'));
 
-import { createStore } from "redux";
+// Self-explanatory
+import { createStore, combineReducers } from "redux";
 
 // When creating the reducer, it should take two arguments: state and action
-const reducer = (state, action) => {
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
     // Action is an object with a type
     // Determine what to do based on the type of action (why we're using a switch statement)
     switch (action.type) {
         case "ADD": 
-            state = state + action.payload;
+            // the below is bad because it is not changing in an immutable way! 
+            // state.result += action.payload;
+
+            // Create a new space in memory for the new state to preserve old states
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                // this does not store the last results but rather stores the last payloads! 
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
         case "SUBTRACT": 
-            state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
     }
     // Must return the new state that the application will use
     return state;
 };
 
+const userReducer = (state = {
+    name: "Max",
+    age: 27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME": 
+            state = {
+                ...state,
+                name: action.payload
+            }
+            break;
+        case "SET_AGE": 
+            state = {
+                ...state,
+                age: action.payload
+            }
+            break;
+    }
+    return state;
+};
+
 // A store initializes with two arguments: the reducer and the initial state
-const store = createStore(reducer, 1);
+const store = createStore(combineReducers({mathReducer, userReducer}));
 
 store.subscribe(() => {
     // getState is built in and gets the current state
@@ -66,4 +104,9 @@ store.dispatch({
 store.dispatch({
     type: "SUBTRACT",
     payload: 20
+});
+
+store.dispatch({
+    type: "SET_AGE",
+    payload: 30
 })
